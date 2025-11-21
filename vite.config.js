9493a3347309a7f5197c27b830bafbb646f5c9d2
Jimmy sim <27620344@sun.ac.za>
2025-11-21 05:54:32 +0200

    Remove feedback proxy from Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// ONLY for local development
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/analyze': 'https://medprompt-backend.onrender.com',
-      '/feedback': 'https://medprompt-backend.onrender.com'
     }
   }
 })
